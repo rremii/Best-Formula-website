a74fd25247bbc42f7495c58ca1fb2745ebe6c38f
rremii <noruto2025@ya.ru>
2021-11-09 19:30:35 +0300

    added some animations for navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,8 +1,18 @@
 import css from './NavBar.module.sass'
+import {useState} from "react";
+
 
 const NavBar = (props) => {
-    return (<div className={css.NavBar}>
-        <div>
+
+    let [isActive, toggleActive] = useState(false)
+
+    const navBar = () => {
+        if (isActive) toggleActive(false)
+        else toggleActive(true)
+    }
+
+    return (<div  className={[css.NavBar, isActive && css.active].join(' ')}>
+        <div onClick={navBar}>
             <span> </span>
             <span> </span>
             <span> </span>
