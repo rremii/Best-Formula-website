cbf9dc3fba6d62480af1f567a44fd7407aa16480
rremii <noruto2025@ya.ru>
2021-12-15 16:30:57 +0300

    started making choosing page

--- src/Redux/ChoosingPageReducer.js
+++ src/Redux/ChoosingPageReducer.js
@@ -2,8 +2,21 @@
 
 let initialState = {
     mathTopics:[
-        {id:1,topic:'hellomath'},
-        {id:2,topic:'hello2math'}
+        {id:1,topic:'posic 1'},
+        {id:2,topic:'topic2'},
+        {id:3,topic:'topic3'},
+        {id:4,topic:'topic4'},
+        {id:5,topic:'topic5'},
+        {id:6,topic:'topic6'},
+        {id:7,topic:'topic7'},
+        {id:8,topic:'topic8'},
+        {id:9,topic:'topic9'},
+        {id:10,topic:'topic10'},
+        {id:11,topic:'topic11'},
+        {id:12,topic:'topic12'},
+        {id:13,topic:'topic13'},
+        {id:14,topic:'topic14'},
+        {id:15,topic:'topic15'},
     ],
     phisicTopics:[
         {id:1,topic:'hellopis'},
@@ -14,8 +27,6 @@ let initialState = {
 
 const choosingPageReducer = (state = initialState, action) => {
     switch (action.type) {
-
-
 
         default:
             return state
--- src/components/ChoosingPage/ChoosingPage.jsx
+++ src/components/ChoosingPage/ChoosingPage.jsx
@@ -1,14 +1,171 @@
 import css from './ChoosingPage.module.sass'
 import {NavLink} from "react-router-dom";
+import {useState} from "react";
 
 const ChoosingPage = (props) => {
     // console.log(props.choosingPage)
+
+    let [current, setCurrent] = useState(1)
+    let [translateTo, setTranslateTo] = useState(100)
+
     return <div className={[css.ChoosingPage].join(' ')}>
-        {props.choosingPage.map(({id, topic}) => {
-            return <div key={id} className={css.topicContainer}>
-                <NavLink to={'' + id}>{topic}</NavLink>
-            </div>
-        })}
+        <div className={css.barrel}>
+            {props.choosingPage.map(({id, topic}) => {
+                let translateBy
+                let isMain = 0
+
+
+//////////////MAIN ONE
+                if (id === current) {
+                    translateBy = 0
+                    isMain = 50
+                }
+                if (id === current + 1) {
+                    translateBy = 100
+                    isMain = 50
+                }
+                if (id === current + 2) {
+                    translateBy = 200
+                    isMain = 50
+                }
+                if (id === current + 3) {
+                    translateBy = 300
+                    isMain = 50
+                }
+                if (id === current - 1) {
+                    translateBy = -100
+                    isMain = 50
+                }
+                if (id === current - 2) {
+                    translateBy = -200
+                    isMain = 50
+                }
+                if (id === current - 3) {
+                    translateBy = -300
+                    isMain = 50
+                }
+
+
+////////////////CURRENT = 1
+                if (current === 1) {
+                    if (id === props.choosingPage.length) {
+                        translateBy = -100
+                        isMain = 50
+                    }
+                    if (id === props.choosingPage.length - 1) {
+                        translateBy = -200
+                        isMain = 50
+                    }
+                    if (id === props.choosingPage.length - 2) {
+                        translateBy = -300
+                        isMain = 50
+                    }
+                }
+/////////////////////CURRENT = 2
+                if (current === 2) {
+                    if (id === 1) {
+                        translateBy = -100
+                        isMain = 50
+                    }
+                    if (id === props.choosingPage.length) {
+                        translateBy = -200
+                        isMain = 50
+                    }
+                    if (id === props.choosingPage.length - 1) {
+                        translateBy = -300
+                        isMain = 50
+                    }
+                }
+////////////////////CURRENT = 3
+                if (current === 3) {
+                    if (id === 2) {
+                        translateBy = -100
+                        isMain = 50
+                    }
+                    if (id === 1) {
+                        translateBy = -200
+                        isMain = 50
+                    }
+                    if (id === props.choosingPage.length) {
+                        translateBy = -300
+                        isMain = 50
+                    }
+                }
+/////////////////////////CURRENT = MAX
+                if (current === props.choosingPage.length) {
+                    if (id === 1) {
+                        translateBy = 100
+                        isMain = 50
+                    }
+                    if (id === 2) {
+                        translateBy = 200
+                        isMain = 50
+                    }
+                    if (id === 3) {
+                        translateBy = 300
+                        isMain = 50
+                    }
+                }
+/////////////////////////////CURRENT = MAX - 1
+                if (current === props.choosingPage.length - 1) {
+                    if (id === props.choosingPage.length) {
+                        translateBy = 100
+                        isMain = 50
+                    }
+                    if (id === 1) {
+                        translateBy = 200
+                        isMain = 50
+                    }
+                    if (id === 2) {
+                        translateBy = 300
+                        isMain = 50
+                    }
+                }
+//////////////////////////////CURRENT = MAX - 2
+                if (current === props.choosingPage.length - 2) {
+                    if (id === props.choosingPage.length - 1) {
+                        translateBy = 100
+                        isMain = 50
+                    }
+                    if (id === props.choosingPage.length) {
+                        translateBy = 200
+                        isMain = 50
+                    }
+                    if (id === 1) {
+                        translateBy = 300
+                        isMain = 50
+                    }
+                }
+
+                const setCurrentId = (id) => {
+                    setCurrent(id)
+                }
+                return <div style={{
+                    // transform: `translateY(${translateBy - 50}%)`,
+                    // animation:  'fade_out'
+                    // top: `${isMain === 50 ? 50 : -50}%`
+                }}
+
+                            key={id}
+                            className={[css.cell,
+                                !translateBy  && translateBy !== 0 ? css.extra : '',
+
+                                translateBy === 0 ? css.middle : '',
+                                translateBy === 100 ? css.secondDown : '',
+                                translateBy === -100 ? css.secondUp : '',
+                                translateBy === 200 ? css.thirdDown : '',
+                                translateBy === -200 ? css.thirdUp : '',
+                                translateBy === 300 ? css.forthDown : '',
+                                translateBy === -300 ? css.forthUp : '',
+                                // translateBy === 300 ? css.fade_out : '',
+                            ].join(' ')}
+                            onClick={() => setCurrentId(id)}
+                >
+
+                    <NavLink className={id !== current ? css.disable : ''} to={'' + id}>{topic}{''}</NavLink>
+                </div>
+            })}
+        </div>
 
     </div>
 
